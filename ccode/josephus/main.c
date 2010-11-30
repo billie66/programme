7f32d2d45c031acb7b39d5d441408d3aa6a160cd
Billie Zhang <billiecoder@gmail.com>
2010-11-30 14:15:19 +0800

    jesephus ring done

--- ccode/josephus/main.c
+++ ccode/josephus/main.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
+#define N 1000
 
 int main(int argc, const char *argv[])
 {
-   int a[10];
-   int i;
-   for (i=0; i<10; i++)
+   int n, m; 
+   printf("Please input the number of people n:\n");
+   scanf("%d", &n);
+   printf("the specific number m:\n");
+   scanf("%d", &m);
+   int i, a[N];
+   for (i=0; i<n; i++)
    {
        a[i]=i+1;
    }
-   for (i=0; i<10; i++)
-   {
-       printf("a[%d]: %d ", i, a[i]);
-   }
-   printf("\n");
-   
-   int j=0, m=3;
-   int k=0, n=1;
-   while(j<10)
+   printf("the sequence of people out of round:\n");
+   int j=0, k=0, count=1;
+   while(j<n)
    {
        if(a[j]!=0)
            k=k+1;
        if(k%m==0&&a[j]!=0)
        {
-           printf("%d: %d out\n ", n++, a[j]);
+           printf("%4d: %4d out ", count, a[j]);
            a[j]=0;
            k=0;
+           if(count%5==0)
+               printf("\n");
+           count++;
        }
-       if(j==9)
+       if(j==(n-1))
            j=0;
        else
            j++;
-       if(n==11)
+       if(count==(n+1))
            break;
    }   
+   printf("\n");
        return 0;
 }
