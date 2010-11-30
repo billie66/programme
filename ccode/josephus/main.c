4ccb23237597828d13ee22f3544d19012071e55a
Billie Zhang <billiecoder@gmail.com>
2010-11-30 15:39:53 +0800

    save time

--- ccode/josephus/main.c
+++ ccode/josephus/main.c
@@ -4,9 +4,9 @@
 int main(int argc, const char *argv[])
 {
    int n, m; 
-   printf("Please input the number of people n:\n");
+   printf("Please input the number of people n(n>0):\n");
    scanf("%d", &n);
-   printf("the specific number m:\n");
+   printf("the specific number m(m>0):\n");
    scanf("%d", &m);
    int i, a[N];
    for (i=0; i<n; i++)
@@ -20,18 +20,20 @@ int main(int argc, const char *argv[])
        if(k%m==0&&a[j]!=0)
        {
            printf("%4d: %4d out ", count, a[j]);
-           a[j]=0;
+           a[j]=0;           // set flag when find the proper member
            k=0;
-           if(count%5==0)
+           if(count%5==0)    // print five pairs of data one line
                printf("\n");
            count++;
        }
        if(j==(n-1))
-           j=0;
+           j=0;             // set j to index the first number in the array
        else
            j++;
-       if(count==(n+1))
+       if(count==n&&a[j]!=0) // when all numbers are zero but one, excute if statement
+       {   printf("%4d: %4d out ", count, a[j]);
            break;
+       }
    }   
    printf("\n");
        return 0;
