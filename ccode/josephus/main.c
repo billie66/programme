f61074722630a7b8cdbd91c1c7dd5c2edb3b4dfe
Billie Zhang <billiecoder@gmail.com>
2010-12-05 17:09:46 +0800

    using K&R style

--- ccode/josephus/main.c
+++ ccode/josephus/main.c
@@ -1,3 +1,7 @@
+/*
+ * Using an array for Joephus problem
+ * Copyright 2010 Billie Zhang
+ */
 #include <stdio.h>
 #define N 1000
 
@@ -9,32 +13,32 @@ int main(int argc, const char *argv[])
    printf("the specific number m(m>0):\n");
    scanf("%d", &m);
    int i, a[N];
-   for (i=0; i<n; i++)
-       a[i]=i+1;
+   for (i = 0; i < n; i++)
+       a[i] = i + 1;
    printf("the sequence of people out of round:\n");
-   int j=0, k=0, count=1;
-   while(j<n)
-   {
-       if(a[j]!=0)
-           k=k+1;
-       if(k%m==0&&a[j]!=0)
-       {
+   int j = k = 0, count = 1;
+   while (j < n) {
+       if (a[j] != 0)
+           k = k + 1;
+       if (k % m == 0 && a[j] != 0) {
            printf("%4d: %4d out ", count, a[j]);
-           a[j]=0;           // set flag when find the proper member
-           k=0;
-           if(count%5==0)    // print five pairs of data one line
+           /* set the proper member kicked out of the group to zero */
+           a[j] = 0;    
+           k = 0;
+           if (count % 5 == 0)       
                printf("\n");
            count++;
        }
-       if(j==(n-1))
-           j=0;             // set j to index the first number in the array
+       if (j == (n-1))
+           j = 0;                
        else
            j++;
-       if(count==n&&a[j]!=0) // when all numbers are zero but one, excute if statement
-       {   printf("%4d: %4d out ", count, a[j]);
+       /* when all numbers are zero but one, excute if statement */
+       if (count == n && a[j] != 0) {
+           printf("%4d: %4d out ", count, a[j]);
            break;
        }
-   }   
+   }
    printf("\n");
-       return 0;
+   return 0;
 }
