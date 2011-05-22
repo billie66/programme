f85783301d6629a85a75e7949523406af50f2ae3
Billie Zhang <billiecoder@gmail.com>
2011-05-22 10:24:08 +0800

    qsort

--- ccode/other/cpp.c
+++ ccode/other/cpp.c
@@ -1,13 +1,25 @@
 /* Token concatenation using "##" preprocessor operator */
 
 #include <stdio.h>
+#include <stdbool.h>  // C99 does define a standard Boolean type.
 
-#define FRUIT(name, num)  name##_##num = num; 
+#define FRUIT(name, num)  name##_##num = num 
 
 int main(int argc, const char *argv[])
 {
     int apple_5;
-    FRUIT(apple, 5)
+    bool flag;
+    struct line {
+        unsigned int a:2;
+    }b;
+
+    printf("b.a: %d\n", b.a);
+
+    FRUIT(apple, 5);
     printf("apple_5: %d\n", apple_5);
+
+    if (flag = true)
+        printf("flag: %d\n", flag);
+
     return 0;
 }
